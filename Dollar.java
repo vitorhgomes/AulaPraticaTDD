6c92df061100c81324cdc3135c8fb836afdc5964
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-07 01:49:55 +0000

    Commit 3

--- Dollar.java
+++ Dollar.java
@@ -1,13 +1,13 @@
-class Dollar {
+class Dollar extends Money{
   private int amount;
 
   Dollar(int amount){
     this.amount= amount;
   }
 
-  Dollar times(int multiplier){
+  Money times(int multiplier)  {
     return new Dollar(amount * multiplier);
-  }
+  }	
 
   public boolean equals(Object object){
     Dollar dollar = (Dollar) object;
--- Franc.java
+++ Franc.java
@@ -1,17 +1,17 @@
-class Franc {   
+class Franc extends Money{   
    private int amount;
 
   Franc(int amount) {      
     this.amount= amount;
   }
 
-  Franc times(int multiplier)  {      
-    return new Franc(amount * multiplier);					
-  }
+  Money times(int multiplier)  {
+    return new Franc(amount * multiplier);
+  }	
 
   public boolean equals(Object object) {					
     Franc franc = (Franc) object;      
     return amount == franc.amount;					
   }
-  					
+
 }
